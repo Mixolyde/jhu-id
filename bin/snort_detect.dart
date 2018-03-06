831a326147cbe91dd75aa5bf11ea5a03c534dd72
Brian Grey <mixolyde@gmail.com>
2018-03-06 02:08:16 +0000

    Initial snort detect

---
+++ bin/snort_detect.dart
@@ -0,0 +1,15 @@
+import 'dart:collection';
+import 'dart:io';
+
+
+void main(List<String> args) {
+  
+  //load file one
+  String truthFile = args[0];
+  String sguilFile = args[1];
+
+  //List inputs = file.readLinSync().split(' ');
+  print ("test: $args");
+
+
+}
