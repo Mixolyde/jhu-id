9d95900538cb8329823a4a03bcf8c52de8dd40f4
Brian Grey <mixolyde@gmail.com>
2018-04-23 00:38:41 +0000

    Multi line bash changes

--- generate_suricata.sh
+++ generate_suricata.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo suricata --user sguil --group sguil -c suricata.yaml -l data -r ~/Downloads/99_w4/ --runmode=autofp -F /etc/nsm/pching-VM-eth1/bpf-ids.conf
+sudo suricata --user sguil --group sguil -c suricata.yaml \
+  -l data -r ~/Downloads/99_w4/ --runmode=autofp \
+  -F /etc/nsm/pching-VM-eth1/bpf-ids.conf
