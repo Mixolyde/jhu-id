7876d0c8eab6fb7f9c256fc7f6b6ddf155e76b06
Brian Grey <mixolyde@gmail.com>
2018-04-23 02:38:36 +0000

    Update argus output for week 4

--- generate_argus.sh
+++ generate_argus.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-argus -i eth0 -F /etc/nsm/pching-VM-eth1/argus.conf \
+echo "" > data/argusout_ports.csv
+
+sudo argus -i eth0 -F /etc/nsm/pching-VM-eth1/argus.conf \
   -w - -r \
   ~/Downloads/99_w4/99_w4_mon_inside.tcpdump \
+  | ra -L -1 -n -F rarc >> data/argusout_ports.csv
+
+sudo argus -i eth0 -F /etc/nsm/pching-VM-eth1/argus.conf \
+  -w - -r \
   ~/Downloads/99_w4/99_w4_wed_inside.tcpdump \
+  | ra -L -1 -n -F rarc >> data/argusout_ports.csv
+
+sudo argus -i eth0 -F /etc/nsm/pching-VM-eth1/argus.conf \
+  -w - -r \
   ~/Downloads/99_w4/99_w4_thu_inside.tcpdump \
+  | ra -L -1 -n -F rarc >> data/argusout_ports.csv
+
+sudo argus -i eth0 -F /etc/nsm/pching-VM-eth1/argus.conf \
+  -w - -r \
   ~/Downloads/99_w4/99_w4_fri_inside.tcpdump \
-  | ra -n -c "," > data/argusout_ports.csv
+  | ra -L -1 -n -F rarc >> data/argusout_ports.csv
+
---
+++ generate_snort.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo snort -c /etc/nsm/pching-VM-eth1/snort.conf \
+  -u sguil -g sguil \
+  -F /etc/nsm/pching-VM-eth1/bpf-ids.conf \
+  -l /nsm/sensor_data/pching-VM-eth1/snort-1 \
+  --perfmon-file /nsm/sensor_data/pching-VM-eth1/snort-1.stats -U \
+  #--pcap-list="/home/pching442/Downloads/99_w4/99_w4_mon_inside.tcpdump"
+  --pcap-list="/home/pching442/Downloads/99_w4/99_w4_wed_inside.tcpdump \
+  /home/pching442/Downloads/99_w4/99_w4_thu_inside.tcpdump \
+  /home/pching442/Downloads/99_w4/99_w4_fri_inside.tcpdump"
